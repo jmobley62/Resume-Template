31856b6775cf062a668ca8be53db35ca3927035b
jmobley62 <jaquan_mobley@yahoo.com>
2022-06-22 21:41:02 -0400

    Resume Template

---
+++ components/HeroSec.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="w-full h-full font-pop text-white">
+        <img src="~static/plant2.jpg" class="absolute w-fit h-fit" style="z-index: -1;">
+        <header class="container flex justify-between items-center pt-12 mx-auto">
+
+            <a href="#" class="logo text-2xl font-black pr-96">Morgan.<span>Agee</span></a>
+
+            <nav class="space-x-12 pl-96">
+                <a href="#">My Story</a>
+                <a href="#">Journal</a>
+                <a href="#">Contact</a>
+            </nav>
+
+            <div class="bg-black">
+                <a href="">IG</a>
+                <a href="">TWITTER</a>
+            </div>
+        </header>
+
+        <section class="flex flex-col justify-center items-center w-screen h-screen">
+        
+            <h1 class="text-9xl text-center mb-10">Lorem ipsum dolor<br>sit amet consectetur.</h1>
+        
+            <button class="p-4 bg-blue-700 text-4xl rounded-2xl">Hire Me</button>
+
+        </section>
+    </div>
+</template>
+
+<style>
+    @import url('https://fonts.googleapis.com/css2?family=Poppins:wght@200;800&display=swap');
+
+    span {
+        color: red;
+    }
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+
+      },
+      fontFamily: {
+        'pop': 'Poppins'
+      }
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
